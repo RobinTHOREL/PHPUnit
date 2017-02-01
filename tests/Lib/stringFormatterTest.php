feb26a7e7a68efa32e85a98159982ca880764b0e
brixton <thorelrobin@yahoo.fr>
2017-02-01 12:38:21 +0100

    test de la classe stringFormatter

--- tests/Lib/stringFormatterTest.php
+++ tests/Lib/stringFormatterTest.php
@@ -12,13 +12,54 @@ namespace tests\Lib;
 
     class stringFormatterTest extends \PHPUnit_Framework_TestCase{
 
-        /*public function testPrefix($prefix, $string, $camelCase = false){
+        public function testPrefix(){
 
+            //mettre en place un contexte
+            $string = new StringFormatter();
 
+            //lancer un programme
+            $response = $string->prefix("pref", "robin");
 
-        }*/
+            //verifier le resultat
+            $this-> assertEquals($response, "prefrobin");
 
-        public function concatStringTest()
+            //lancer un programme
+            $response1 = $string->prefix("pref", "robin", true);
+
+            //verifier le resultat
+            $this-> assertEquals($response1, "prefRobin");
+        }
+
+        public function testSuffix()
+        {
+            //mettre en place un contexte
+            $string = new StringFormatter();
+
+            //lancer un programme
+            $response = $string->suffix("suff", "robin");
+
+            //verifier le resultat
+            $this->assertEquals($response, "robinsuff");
+
+            $response1 = $string->suffix("suff", "Robin", true);
+
+            $this->assertEquals($response1, "robinSuff");
+
+        }
+
+        public function testToCamelCase()
+        {
+            //mettre en place un contexte
+            $string = new StringFormatter();
+
+            //lancer un programme
+            $response = $string->toCamelCase("robin", "thorel");
+
+            //verifier le resultat
+            $this-> assertEquals($response, "robinThorel");
+        }
+
+        public function testConcatString()
         {
             //mettre en place un contexte
             $string = new StringFormatter();
@@ -27,10 +68,8 @@ namespace tests\Lib;
             $response = $string->concatString("c un", "test");
 
             //vérifier le resultat
-            $this-> assertEquals($response, "c un test");
+            $this-> assertEquals($response, "c untest");
 
         }
-
-
 
 }
